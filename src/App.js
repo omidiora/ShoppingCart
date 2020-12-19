4de1c748a71e0fc5f0fbed28812f6e8fc4671921
omidiora <omidioraemmanuel@gmail.com>
2020-12-19 19:44:43 +0100

    sort and filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Filter from './components/Filter';
+import Products from './components/Products';
+import data from './data.json';
 
-function App() {
+class App  extends  React.Component{
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       products:data.products,
+       size:"",
+       sort:"",
+    }
+  }
+  sortProducts=(event)=>{
+    
+    console.log(event.target.value);
+    const sort=event.target.value;
+    this.setState({ 
+      sort:event.target.value , 
+      products:this.state.products.slice().sort((a,b)=>(
+        sort==='lowest' ? ((a.price > b.price) ? 1:-1):
+        sort==='highest'?((a.price < b.price) ? 1:-1):
+        ((a._id < b._id) ? 1:-1)
+      ))
+    })
+
+  }
+ 
+ filterProducts = (event) => {
+    // impl
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } 
+    else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+
+      <header>
+        <a href='/'>React Shopping Cart</a>
       </header>
+      <main>
+       <div className='content'>
+         <div className='main'>
+           <Filter count={this.state.products.length} size={this.state.size} sortProduct={this.state.sortProducts} filterProducts={this.filterProducts} 
+           sortProduct={this.sortProducts} ></Filter>
+           
+           <Products products={this.state.products} />
+         </div>
+         <div className='sidebar' >Cart Items</div>
+       </div>
+      </main>
+        <footer>
+          All right is resevered
+        </footer>
+    
+      
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/Filter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export class Filter extends Component {
+    render() {
+        return (
+            <div className='filter'>
+                <div className='filter-result'>{this.props.count}  Products</div>
+                <div className='filter-sort'>
+                    {""} Order
+                    <select value={this.props.size} onChange={this.props.sortProduct}>
+                    <option>Latest</option>
+                    <option value='lowest'>Lowest</option>
+                    <option value='highest'>Highest</option>
+                    </select>
+                </div>
+                <div className='filter-size'>
+                    Filter
+                </div>
+                <select value={this.props.size} onChange={this.props.filterProducts}>
+                <option value=''>All</option>
+                <option value='XS'>XS</option>
+                <option value='S'>S</option>
+                <option value='M'>M</option>
+                <option value='L'>L</option>
+                
+                </select>
+
+
+          
+            </div>
+        )
+    }
+}
+
+export default Filter
